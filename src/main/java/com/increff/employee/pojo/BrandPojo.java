4055bfc8b406e034aae7b87b1471ee25359d37f5
Abhishek Chaudhary <abhishekchaudhary@N0467-AbhishekC.local>
2023-06-19 22:10:45 +0530

    model

--- src/main/java/com/increff/employee/pojo/BrandPojo.java
+++ src/main/java/com/increff/employee/pojo/BrandPojo.java
@@ -1,6 +1,14 @@
 package com.increff.employee.pojo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class BrandPojo {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
     private String brand;
     private String category;
