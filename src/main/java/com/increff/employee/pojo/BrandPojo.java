b311755a72a055404ed1f4133c7079d35ca9cd96
Abhishek Chaudhary <abhishekchaudhary@N0467-AbhishekC.local>
2023-06-19 19:28:40 +0530

    First Commit

---
+++ src/main/java/com/increff/employee/pojo/BrandPojo.java
@@ -0,0 +1,31 @@
+package com.increff.employee.pojo;
+
+public class BrandPojo {
+    private int id;
+    private String brand;
+    private String category;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
