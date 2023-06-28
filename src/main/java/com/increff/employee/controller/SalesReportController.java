a044d9e71e2500ae669d99aa08c79e1196ec58dd
Abhishek Chaudhary <abhishekchaudhary@N0467-AbhishekC.local>
2023-06-28 09:01:56 +0530

    not much change

---
+++ src/main/java/com/increff/employee/controller/SalesReportController.java
@@ -0,0 +1,30 @@
+//package com.increff.employee.controller;
+//
+//import com.increff.employee.model.ProductData;
+//import com.increff.employee.pojo.ProductPojo;
+//import com.increff.employee.service.ApiException;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Api
+//@RestController
+//public class SalesReportController {
+//    @ApiOperation(value = "Gets list of all Products")
+//    @RequestMapping(path = "/api/brandreport", method = RequestMethod.GET)
+//    public List<ProductData> getBrandReport() throws ApiException {
+//
+//    }
+//
+//    @ApiOperation(value = "Gets list of all Products")
+//    @RequestMapping(path = "/api/inventoryreport", method = RequestMethod.GET)
+//    public List<ProductData> getInventoryReport() throws ApiException {
+//
+//    }
+//
+//}
